4dbb50c7f4a6bb2c21dd0cdf558831fb3a39b57c
arvandroogen <arthur.vandroogenbroek@student.uclouvain.be>
2020-11-09 23:12:30 +0100

    script bash + producer consumer

--- src/producer_consumer.c
+++ src/producer_consumer.c
@@ -3,39 +3,37 @@
 #include<stdint.h>
 #include <semaphore.h>
 #include <pthread.h>
+#include <time.h>
 #define N 8
 #define SIZE 1024
 int buffer[N];
-int isEmpty[N];
-int cons=0;
-int prod=0;
-sem_t empty;
-sem_t full;
-pthread_mutex_t mutex;
-pthread_mutex_t a;
-pthread_mutex_t b;
-
+int isEmpty[N];// array to know which case of the buffer is full
+int cons=0;// number of data already consumed
+int prod=0;// number of data already produceed
+sem_t empty;// semaphore, val = nbr of empty case
+sem_t full;// semaphore, val = nbr of not empty case
+pthread_mutex_t mutex;// buffer's mutex
 void work(){
-    ;
-    //while(rand()>10000);
+    clock_t start=clock();
+    while(rand()>RAND_MAX/10000);
 }
 
 void* producer(void* param){
     while(1){
-        sem_wait(&empty);
+        sem_wait(&empty); //Wait or fill an empty case
         work();
         int a=rand()-rand();
         pthread_mutex_lock(&mutex);
         if(prod>=SIZE) break;
         int i=0;
-        while(!isEmpty[i]) i++;
+        while(!isEmpty[i]) i++;//find an empty place
         buffer[i]=prod;
         isEmpty[i]=0;
         prod++;
         pthread_mutex_unlock(&mutex);
-        sem_post(&full);
+        sem_post(&full);// say to consumer than one more case is full
     }
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex);//unlock the buffer because it wasn't close because of the break
     sem_post(&full);
     return NULL;
 }
@@ -62,32 +60,39 @@ void *consumer(void *param){
 
 int main(int argc, char const *argv[])
 {
-    pthread_t thread_prod[3];
-    pthread_t thread_cons[3];
+    //Create the number of threads requested
+    int nbrProd=atoi(argv[1]); 
+    int nbrCons=atoi(argv[2]);
+    //printf("prod : %d   cons : %d\n",nbrProd,nbrCons);
+    pthread_t thread_prod[nbrProd];
+    pthread_t thread_cons[nbrCons];
+
+    // Initialisation
     sem_init(&full,0,0);
     sem_init(&empty,0,N);
     pthread_mutex_init(&mutex,NULL);
     for (int i = 0; i <N ; ++i) {
         isEmpty[i]=1;
     }
-    for (int i = 0; i < 3; i++)
+
+    //Start threads(producers then consumers)
+    for (int i = 0; i < nbrProd; i++)
     {
         pthread_create(&thread_prod[i],NULL,&producer,NULL);
     }
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < nbrCons; i++)
     {
         pthread_create(&thread_cons[i],NULL,&consumer,NULL);
     }
-    printf("threads created\n");
-    for (int i = 0; i < 3; i++)
+
+    //Wait that threads have finished
+    for (int i = 0; i < nbrProd; i++)
     {
         pthread_join(thread_prod[i],NULL);
     }
-    printf("Producers finished\n");
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < nbrCons; i++)
     {
         pthread_join(thread_cons[i],NULL);
     }
-    printf("Consumers finished\n");
     return 0;
 }
