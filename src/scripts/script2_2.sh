fd8bd7c4abf809ae6062112de2c7c2b6eeff38d9
mvanmollekot <martin.vanmollekot@student.uclouvain.be>
2020-11-30 09:28:38 +0100

    minor changes scipt tache2_2 (5 répétitions)

--- src/scripts/script2_2.sh
+++ src/scripts/script2_2.sh
@@ -2,21 +2,27 @@
 cd output/T22
 echo "Nbr threads,Time">../../src/CSV/Tache2_2/ts.csv
 for i in $(seq 1 16);do
-    echo -n "$i,">>../../src/CSV/Tache2_2/ts.csv
-    /usr/bin/time -ao ../../src/CSV/Tache2_2/ts.csv -f %e ./ts.out $(($i))
+    for i in {1..5};do
+        echo -n "$i,">>../../src/CSV/Tache2_2/ts.csv
+        /usr/bin/time -ao ../../src/CSV/Tache2_2/ts.csv -f %e ./ts.out $(($i))
+    done
 done
 echo "Tache 2.2 done"
 
 echo "Nbr threads,Time">../../src/CSV/Tache2_2/tts.csv
 for i in $(seq 1 16);do
-    echo -n "$i,">>../../src/CSV/Tache2_2/tts.csv
-    /usr/bin/time -ao ../../src/CSV/Tache2_2/tts.csv -f %e ./tts.out $(($i))
+    for i in {1..5};do
+        echo -n "$i,">>../../src/CSV/Tache2_2/tts.csv
+        /usr/bin/time -ao ../../src/CSV/Tache2_2/tts.csv -f %e ./tts.out $(($i))
+    done
 done
 echo "Tache 2.2 done"
 
 echo "Nbr threads,Time">../../src/CSV/Tache2_2/btts.csv
 for i in $(seq 1 16);do
-    echo -n "$i,">>../../src/CSV/Tache2_2/btts.csv
-    /usr/bin/time -ao ../../src/CSV/Tache2_2/btts.csv -f %e ./btts.out $(($i))
+    for i in {1..5};do
+        echo -n "$i,">>../../src/CSV/Tache2_2/btts.csv
+        /usr/bin/time -ao ../../src/CSV/Tache2_2/btts.csv -f %e ./btts.out $(($i))
+    done
 done
 echo "Tache 2.2 done"
